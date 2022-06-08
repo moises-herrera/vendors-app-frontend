e76cdbcff525339b7d37b01196eecd8ec5a88438
moises-herrera <moisesherrera037@gmail.com>
2022-06-07 20:16:59 -0500

    added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1e81ce",
+        secondary: "#0e6cb5",
+      },
+    },
   },
   plugins: [],
 };
